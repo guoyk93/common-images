6dd4e8bfd1817640f87fba07c3e8798f996d0b55
Guo Y.K <hi@guoyk.net>
2020-06-23 18:22:11 +0800

    split build and deploy

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ source ./manifest.sh
 
 for REPO in ${REPOS[@]}; do
     echo $REPO
-    docker build -t guoyk/$REPO $REPO
+    docker push guoyk/$REPO
 done
---
+++ manifest.sh
@@ -0,0 +1,5 @@
+export REPOS=(
+    common-ubuntu:20.04
+    common-jdk:11
+    common-elasticsearch:7.5.2
+)
