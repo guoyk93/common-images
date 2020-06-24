8d1e3ab809926a789f5ccb68ec083dfcbbca9383
Guo Y.K <hi@guoyk.net>
2020-06-24 11:25:05 +0800

    add common-alpine:3.11

--- manifest.sh
+++ manifest.sh
@@ -1,6 +1,9 @@
 export REPOS=(
+    # ubuntu series
     common-ubuntu:20.04
     common-jdk:11
     common-elasticsearch:7.5.2
     common-kibana:7.5.2
+    # alpine series
+    common-alpine:3.11
 )
